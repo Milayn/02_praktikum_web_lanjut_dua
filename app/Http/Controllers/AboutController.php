5127f0e62da47220bb5e6565aa9fb2cc355474d2
Milayn <millayunita07@gmail.com>
2022-03-04 00:00:53 +0700

    praktikum 2 js3

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about(){
+        return 'Mila Yunita<br> 2041720027';
+    }
+}
